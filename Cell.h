91d28f4ce591a347b012c1ca5ccdf94e79383413
Christian Raul Delgado Diaz de Leon <christian.delgado-diaz-de-leon@insa-lyon.fr>
2017-02-27 16:30:04 +0100

    cell update

--- Cell.h
+++ Cell.h
@@ -48,6 +48,7 @@ virtual float Metabolic_Network(float i) = 0;
 //==============================
 float* Phenotype;
 float Fitness;
+static float W_min;
 static float death_Probability;
 static float mutation_Probability;
 };

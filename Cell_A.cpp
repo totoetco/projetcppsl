44bb29f6bf6bd5f59d591fb17f77357033eb37d4
ThomasB <thomas.breniere@insa-lyon.fr>
2017-02-27 16:36:08 +0100

    manage fitness

--- Cell_A.cpp
+++ Cell_A.cpp
@@ -31,5 +31,9 @@ float Cell_B::Metabolic_Network(float A_out){
 	A_out = A_out*(1-RAA_);
 	Phenotype[0] = Phenotype[0]*(1-RAB_) + A_out * RAA_;
 	Phenotype[1] = Phenotype[1] + Phenotype[0] * RAB_;
+
+	//màj fitness
+	Fitness = Phenotype[1];
+	if(Fitness < W_min) Fitness = 0;
 	return A_out;
 }

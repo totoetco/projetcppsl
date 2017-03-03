92609a1f6f8d961d25c21a6877ea59abad3bd45b
ThomasB <thomas.breniere@insa-lyon.fr>
2017-03-03 17:24:36 +0100

    major changes in environment creation

--- Environment.cpp
+++ Environment.cpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <time.h>
 using std::cout;
 using std::endl;
 
@@ -17,13 +18,29 @@ using std::endl;
 //    CONSTRUCTORS
 //==============================
 Environment::Environment(){
+	srand(time(NULL));
 	T_ = 1000;
+	int Max_ = (W_ * H_)/2;
+	int comptA_ = 0;
+	int comptB_ = 0;
    Gap **themap = new Gap*[H_];
    for (int i = 0; i < H_; i++){
       themap[i] = new Gap[W_];
+      for (int j = 0; j < W_; j++){
+      	bool randbool = rand() & 1;
+      	if (comptA_ <= Max_ && comptB_ <= Max_){
+      		themap[i][j].Set_Gap(i,j,randbool);
+      	} else if (comptA_ == Max_){
+      		themap[i][j].Set_Gap(i,j,false);
+      	} else {
+      		themap[i][j].Set_Gap(i,j,true);
+      	}
+      }	
    }
+   for (int i = 0; i < H_; i++)
+      delete[] themap[i];
+   delete[] themap;
 
-   // utiliser tableau2D...
 	cout << "I have created an Environment" << endl;
 }
 //==============================

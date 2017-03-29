45705c204dd9dff85a22b17e40995921f5a164e1
Thomas B <thomas.breniere@insa-lyon.fr>
2017-03-29 22:36:19 +0200

    Big changes to make a nice heatmap

--- extractor.R
+++ extractor.R
@@ -1,5 +1,22 @@
-install.packages("plotly")
 library(plotly)
-packageVersion('plotly')
 a = read.table("output.txt", dec = ".", header = TRUE,sep = ",")
-p <- plot_ly(z = volcano, type = "heatmap")
+col1 = a$t_value
+col2 = a$A_init
+col3 = a$ratiocellfinal
+col1.sorted <- col1[order(col1)]
+col2.sorted <- col2[order(col1)]
+col3.sorted <- col3[order(col1)]
+m <- matrix(nrow = length(unique(col1.sorted)), ncol = length(unique(col2.sorted)))
+dimnames(m) <- list(unique(col1.sorted), unique(col2.sorted))
+lengthtot <- 1
+for(i in 1:length(unique(col1.sorted))){ # de 1 à 2
+  for(j in 1:length(unique(col2.sorted))){ # de 1 à 4
+    m[i,j] = col3.sorted[lengthtot]
+    lengthtot <- lengthtot+1
+  }
+}
+colfunc <- colorRampPalette(c("yellow","red"))
+colfunc(10)
+plot(rep(1,10),col=colfunc(10),pch=19,cex=3)
+heatmap(m,Rowv = NA, Colv = NA,main = "Magnifique Plot",col = colfunc(5))
+                                                          
